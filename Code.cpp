752dd3b9104ae7f6da619f049569502aea043e08
vtleavs <vleavs@gmail.com>
2017-09-22 11:33:33 -0400

    Fleshed out Code.h and Code.cpp; Variables are now usable as multiple types; Added some output strings for Classes, Functions and Instructions

--- Code.cpp
+++ Code.cpp
@@ -3,6 +3,62 @@
 
 #include "Code.h"
 
+void Instruction::compile()
+{
+    compiledString = interpret(instructionString);
+}
 
+void Function::compile()
+{
+    for(Instruction i : instructions)
+    {
+        i.compile();
+        compiledString += i.getCompiled() + "\n";
+    }
+    compiledString += "}\n\n";
+}
+
+void Function::writeHeader(std::string className)
+{
+    compiledString += functionType + className "::" + functionName + "("
+    for(for int i = 0; i < arguments.getSize()-1; ++i)
+        compiledString += arguments[i].getType() + " " + arguments[i].getName() + ",";
+    compiledString += arguments[arguments.getSize()].getType() + " "
+        + arguments[arguments.getSize()].getName() + "){\n";
+}
+
+void Function::writeHeader()
+{
+    compiledString += functionType + functionName + "("
+    for(for int i = 0; i < arguments.getSize()-1; ++i)
+        compiledString += arguments[i].getType() + " " + arguments[i].getName() + ",";
+    compiledString += arguments[arguments.getSize()].getType() + " "
+        + arguments[arguments.getSize()].getName() + "){\n";
+}
+
+void Class::compile()
+{
+    compiledString += "class " + className + "{\n";
+    compiledString += "public:\n";
+    for(Function f : methods)
+    {
+        f.compile();
+        f.writeHeader(className);
+        compiledString += f.getCompiled() + "\n";
+    }
+    for(Variable v : members)
+    {
+        compiledString += v.getType() + " get" + v.getName() + "() "
+            + "{ return " + v.getName() + "; }\n";
+        compiledString += "void set" + v.getName() + "(" + v.getType() + "k) "
+            + "{ " + v.getName() + "= k; }\n";
+    }
+    compiledString += "private:\n";
+    for(Variable v : members)
+    {
+        compiledString += v.getType() + " " + v.getName() + ";\n";
+    }
+    compiledString += "};\n\n";
+}
 
 #endif
--- Code.h
+++ Code.h
@@ -4,15 +4,29 @@
 #include <vector>
 #include <string>
 
+#include "interpreter.h"
+
 class Variable
 {
     public:
-        Variable(std::string name, int value){}
-        Variable(std::string name, char value){}
-        Variable(std::string name, bool value){}
-        Variable(std::string name, std::string value){}
-    private:
+        Variable(std::string name, int value): valueInt(value) {}
+        Variable(std::string name, char value): valueChar(value) {}
+        Variable(std::string name, bool value): valueBool(value) {}
+        Variable(std::string name, float value): valueFloat(value) {}
+        Variable(std::string name, std::string value): valueString(value) {}
 
+        int getValue(int x){ return valueInt; }
+        bool getValue(bool x){ return valueBool; }
+        std::string getValue(std::string x){ return valueString; }
+        char getValue(char x){ return valueChar; }
+        float getValue(float x){ return valueFloat; }
+
+    private:
+        std::string valueString;
+        float valueFloat;
+        int valueInt;
+        char valueChar;
+        bool valueBool;
 };
 
 class Instruction
@@ -21,30 +35,57 @@ class Instruction
         Instruction(std::string instString): instructionString(instString) {}
 
         std::string getString() { return instructionString; }
+
+        void compile();
+        std::string getCompiled() { return compiledString; }
     private:
         std::string instructionString;
+        std::string compiledString;
 };
 
 class Function
 {
     public:
-        Function(std::vector<Variable> arguments){
+        Function(std::string name,
+            std::string returnType,
+            std::vector<Variable> arguments): functionName(name), functionType(returnType)
+        {
             localVariables.insert( localVariables.end(),
                 arguments.begin(),
                 arguments.end() );
         }
+
+        void compile();
+        std::string getCompiled() { return compiledString; }
+
+        void writeHeader(std::string className);
+        void writeHeader();
+
+        void addInstruction(Instruction ins) { instructions.push_back(ins); }
+
     private:
-        std::vector<Instruction> includes;
+        std::vector<Instruction> instructions;
         std::vector<Variable> localVariables;
+        std::string compiledString;
+        std::string functionType;
+        std::string functionName;
 };
 
 class Class
 {
     public:
         Class(){}
+
+        void compile();
+        std::string getCompiled() { return compiledString; }
+
+        void addMethod(Function f) { methods.push_back(f); }
+        void addMember(Variable v) { members.push_back(v); }
+
     private:
         std::vector<Variable> members;
         std::vector<Function> methods;
+        std::string compiledString;
 
 };
 
